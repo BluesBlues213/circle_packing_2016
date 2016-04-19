3fb83619b7375f42b548b0ddb4c8050c8d50d32f
Ti Xu <ti.xu.213@gmail.com>
2016-04-19 13:24:28 +0100

    Update pack_v.m

--- pack_v.m
+++ pack_v.m
@@ -1,14 +1,13 @@
 function [NWP,NWR] = pack_v(f)
-% circle packing in square square, January 22nd 2015
-pool=f; % pool of radii (microns)
+
+pool=f; 
 mm=mean(pool);
 div=mm/(10^-5);
 pool=pool/div;
-
 area=sum(pi*(pool.^2))/0.9;
 side=sqrt(area);
-res=1400; % usually between 1000 to 1500
-%% meshgrid
+res=1400; 
+
 ST=side/res;
 [x y]=meshgrid(0:ST:side);
 p0=[side/2,side/2];
@@ -24,7 +23,7 @@ R1=zeros(size(x,1),size(y,1));
 i=1;
 j=1;
 MASK=zeros(size(x));
-%% prep1
+
 px=Wp(:,1);
 py=Wp(:,2);
 down=0.0*10^-4;
@@ -32,7 +31,6 @@ up=side;
 t=linspace(down,up,res+1);
 SC=t(5)-t(4);
 [x y]=meshgrid(down:SC:up , down:SC:up);
-%% prep2
 routter=Wr.*(1);
 
 W=zeros(size(x));
@@ -47,8 +45,6 @@ for i=1:numel(Wr)
     
 end
 MASK11=W;
-
-%%
 while i<=numel(pool)
     
     tic;
@@ -59,12 +55,6 @@ while i<=numel(pool)
     
     d1=sqrt( (x-avg_center(1)).^2 + (y-avg_center(2)).^2 );
     d_t=d1;
-    
-    % Use matlab's imdilate function to generate an exclusion zone within
-    % which the circle-to-be-placed can not be put. And then position this
-    % circle just outside this boundary and closest to the mean center of
-    % the packing.
-    
     m_M=zeros(size(x,1),size(y,1));
     for n=1:numel(WR)
         w=sqrt( (x-WPT(n,1)).^2 + (y-WPT(n,2)).^2 ) <= ...
@@ -96,21 +86,19 @@ while i<=numel(pool)
     toc
 end
 
-%% remove repeats
+
 idx= ~(WPT(:,1)==0 & WPT(:,2)==0);
 NWP=WPT(idx,:);
 NWR=WR(idx);
-%% rescale
 NWP=NWP*div;
 NWR=NWR*div;
-%% recheck
+
 close all;
 figure;axis square;title('packing');hold on
 for i=1:numel(NWR);
     circle(NWP(i,:),NWR(i));
 end
 
-%% check for size vs distance to center bias
 px=NWP(:,1);
 py=NWP(:,2);
 cx=mean(px);
